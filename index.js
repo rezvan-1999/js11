e9e4d8bee1b3d1ecb08d0c01a0f393af3a381f57
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-08 07:07:13 -0800

    the answer of q4

---
+++ index.js
@@ -0,0 +1,15 @@
+// ? 4. Change two or more values of this object .
+// todo : one of the values that you change must be the skills property // example: ['python', 'css', 'js']
+
+const obj2 = {
+  id: "kd455",
+  age: 24,
+  username: "john2000",
+  skills: ["html", "css", "js"],
+};
+
+//  !   Answer:
+obj2.skills = ["c", "c++", "c#"];
+obj2.id = "rezdev";
+obj2.username = "Rezvan Banoo";
+console.log(obj2);
