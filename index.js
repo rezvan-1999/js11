94ff1eb9635a9d20c8c6a51227a8aa91b53dcdeb
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-09 04:47:34 -0800

    the answer of q8

---
+++ index.js
@@ -0,0 +1,11 @@
+// ? 8. Use Array Destructuring to get all of the names from this Nested Array
+// hint: Search about destructuring nested arrays.
+
+const moreStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
+
+//! Answer :
+
+const [first, [second, third], [forth, fifth]] = moreStudents;
+console.log(
+  first + " , " + second + " , " + third + " , " + forth + " , " + fifth
+);
