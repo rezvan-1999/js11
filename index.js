5f05608c888b1c579abf3bfb634d8dc32ed8face
rezvan khalili <rezvan.kh.1999@gmail.com>
2024-12-10 00:13:13 -0800

    the answer of q14

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
  todo: 1. Take the object from question 1  
  todo: 2. put all the values into an array with 2 ways that you’ve learned
 */
+
+const obj = { number: 6043, age: 21, pt: 96 };
+
+// first way
+const firstWay = Object.values(obj);
+console.log(firstWay);
+
+
+
+// second way
+let result = [];
+for (const values in obj) {
+  let getValues = obj[values];
+  result.push(getValues);
+}
+
+console.log(result);
